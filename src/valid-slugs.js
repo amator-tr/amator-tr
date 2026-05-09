3d31212c19df7943594f288f9506db8fd3607804
Admin Publish <admin@amator.tr>
2026-05-09 23:28:16 +0000

    unpublish: test (tutorial) by kaandikec

--- src/valid-slugs.js
+++ src/valid-slugs.js
@@ -83,7 +83,6 @@ export const VALID_TUTORIAL_SLUGS = new Set([
   "swl-dinleyici-disiplini",
   "swr-temel-bilgisi",
   "telsiz-guc-kaynaklari-rehberi",
-  "test",
   "toprak-dalgalari-haberlesme",
   "trac-amator-radyo-turkiye-tarihi",
   "universite-amator-radyo-kulupleri",
